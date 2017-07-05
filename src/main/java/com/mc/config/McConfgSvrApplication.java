5104df916bf02999d9b5f172ad42bde9a9fb8659
santhoshpr <santhosh.ramachandran@ge.com>
2017-07-05 19:46:11 -0400

    initial commit

---
+++ src/main/java/com/mc/config/McConfgSvrApplication.java
@@ -0,0 +1,14 @@
+package com.mc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class McConfgSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McConfgSvrApplication.class, args);
+	}
+}
